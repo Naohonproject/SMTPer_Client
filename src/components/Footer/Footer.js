1278043e6a56d5a793c201da59ae1b9044147d28
letuanbao <letuanbao@mbg8.onmicrosoft.com>
2022-11-07 09:34:28 +0700

    modify footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import style from "./style.module.css";
 
 const Footer = () => {
   return (
-    <footer className={style.footer}>
+    <footer className={style.footer + " " + "font-sans"}>
       SMTPer v0.410 (2010 - 2022), Online SMTP test tool to send and check your email server
     </footer>
   );
